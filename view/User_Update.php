87aa920a504ce15d110e49169c11b3a3c738974d
MaValentina <mariapinedabarroeta@gmail.com>
2022-05-17 18:40:52 -0400

    headers

--- view/User_Update.php
+++ view/User_Update.php
@@ -12,71 +12,52 @@
 </head>
 
 <body>
-    <header class="header">
+<header class="header">
 
-        <a class="header__name" href="../index.php">
-            <h1 class="header__name">Battle Paint</h1>
+<a class="header__name" href="../index.php">
+  <h1 class="header__name">Battle Paint</h1>
+</a>
+
+<nav class="menu__container">
+  <ul class="menu">
+    <div class="menu__select">
+      <li class="menu__item">
+        <a href="marketplace.php" title="Buy">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Buy
         </a>
+      </li>
 
-        <nav class="menu__container">
+      <li class="menu__item">
+        <a href="marketplace.php" title="Rent">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Rent
+        </a>
+      </li>
 
-            <ul class="menu">
+      <li class="menu__item">
+        <a href="points.php" title="Conversion">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Points
+        </a>
+      </li>
+    </div>
 
-                <div class="menu__select">
-
-                    <li class="menu__item">
-                        <a href="marketplace.php" title="Buy">
-                            <span class="neon1"></span>
-                            <span class="neon2"></span>
-                            <span class="neon3"></span>
-                            <span class="neon4"></span> Buy
-                        </a>
-                    </li>
-
-                    <li class="menu__item">
-                        <a href="marketplace.php" title="Rent">
-                            <span class="neon1"></span>
-                            <span class="neon2"></span>
-                            <span class="neon3"></span>
-                            <span class="neon4"></span> Rent
-                        </a>
-                    </li>
-
-                    <li class="menu__item">
-                        <a href="points.php" title="Points">
-                            <span class="neon1"></span>
-                            <span class="neon2"></span>
-                            <span class="neon3"></span>
-                            <span class="neon4"></span> Points
-                        </a>
-                    </li>
-                </div>
-
-                <div class="menu__user">
-
-                    <li class="menu__item">
-                        <a href="#" title="Support">
-                            <span class="neon1"></span>
-                            <span class="neon2"></span>
-                            <span class="neon3"></span>
-                            <span class="neon4"></span> Support
-                        </a>
-                    </li>
-
-                    <li class="menu__item">
-                        <a href="marketplace.php" title="Connect">
-                            <span class="neon1"></span>
-                            <span class="neon2"></span>
-                            <span class="neon3"></span>
-                            <span class="neon4"></span> Marketplace
-                        </a>
-                    </li>
-
-                </div>
-            </ul>
-        </nav>
-
-    </header>
+    <div class="menu__user">
+      <li class="menu__item_admin" >
+        <a href="./user.php"><img src="../source/img/svg/user.svg" alt="Logo Admin" class="logo_admin"></a>
+        <h5>$usuario</h5>
+      </li>
+    </div>
+  </ul>
+</nav>
+</header>
     <main class="main">
         <form action="" class="update" id="update">
             <section class="register">
--- view/register_nft.php
+++ view/register_nft.php
@@ -11,76 +11,52 @@
 </head>
 
 <body>
-  <header class="header">
+<header class="header">
 
-    <a class="header__name" href="../index.php">
-      <h1 class="header__name">Battle Paint</h1>
-    </a>
+<a class="header__name" href="../index.php">
+  <h1 class="header__name">Battle Paint</h1>
+</a>
 
-    <nav class="menu__container">
+<nav class="menu__container">
+  <ul class="menu">
+    <div class="menu__select">
+      <li class="menu__item">
+        <a href="marketplace.php" title="Buy">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Buy
+        </a>
+      </li>
 
-      <ul class="menu">
+      <li class="menu__item">
+        <a href="marketplace.php" title="Rent">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Rent
+        </a>
+      </li>
 
-        <div class="menu__select">
+      <li class="menu__item">
+        <a href="points.php" title="Conversion">
+          <span class="neon1"></span>
+          <span class="neon2"></span>
+          <span class="neon3"></span>
+          <span class="neon4"></span> Points
+        </a>
+      </li>
+    </div>
 
-          <li class="menu__item">
-            <a href="marketplace.php" title="Buy">
-              <span class="neon1"></span>
-              <span class="neon2"></span>
-              <span class="neon3"></span>
-              <span class="neon4"></span>
-              Buy
-            </a>
-          </li>
-
-          <li class="menu__item">
-            <a href="marketplace.php" title="Rent">
-              <span class="neon1"></span>
-              <span class="neon2"></span>
-              <span class="neon3"></span>
-              <span class="neon4"></span>
-              Rent
-            </a>
-          </li>
-
-          <li class="menu__item">
-            <a href="points.php" title="Conversion">
-              <span class="neon1"></span>
-              <span class="neon2"></span>
-              <span class="neon3"></span>
-              <span class="neon4"></span>
-              Points
-            </a>
-          </li>
-        </div>
-
-        <div class="menu__user">
-          
-          <li class="menu__item">
-            <a href="#" title="Support">
-              <span class="neon1"></span>
-              <span class="neon2"></span>
-              <span class="neon3"></span>
-              <span class="neon4"></span>
-              Support
-            </a>
-          </li>
-
-          <li class="menu__item">
-            <a href="Login_Page.php" title="Connect">
-              <span class="neon1"></span>
-              <span class="neon2"></span>
-              <span class="neon3"></span>
-              <span class="neon4"></span>
-              Login
-            </a>
-          </li>
-
-        </div>
-      </ul>
-    </nav>
-
-  </header>
+    <div class="menu__user">
+      <li class="menu__item_admin" >
+        <a href="./admin.php"><img src="../source/img/svg/admin.svg" alt="Logo Admin" class="logo_admin"></a>
+        <h5>Admin</h5>
+      </li>
+    </div>
+  </ul>
+</nav>
+</header>
   
   
   <main class="main">
